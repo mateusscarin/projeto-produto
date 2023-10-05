649393d1a2879f6954f7652c0a887d0d953f482c
mateusscarin <mateus.scarin@gmail.com>
2023-10-04 21:06:41 -0300

    feat: controller Cadastrar Produto

---
+++ src/java/br/com/produto/controller/CadastrarProduto.java
@@ -0,0 +1,88 @@
+package br.com.produto.controller;
+
+import br.com.produto.dao.GenericDAO;
+import br.com.produto.dao.ProdutoDAOImpl;
+import br.com.produto.model.Produto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mateus Scarin
+ */
+@WebServlet(name = "CadastrarProduto", urlPatterns = {"/CadastrarProduto"})
+public class CadastrarProduto extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String nome = request.getParameter("nomeproduto");
+        String descricao = request.getParameter("descricaoproduto");
+
+        String mensagem;
+
+        Produto produto = new Produto();
+        produto.setDescricao(descricao);
+        produto.setNome(nome);
+
+        try {
+            GenericDAO<Produto> dao = new ProdutoDAOImpl();
+            if (dao.cadastrar(produto)) {
+                mensagem = "Produto cadastrado com sucesso!";
+            } else {
+                mensagem = "Problema ao cadsatrar produto!";
+            }
+            request.setAttribute("mensagem", mensagem);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        } catch (Exception e) {
+            System.out.println("Problema no servlet ao cadastrar produto. "
+                    + "\nErro: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
